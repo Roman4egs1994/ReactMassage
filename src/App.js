bf483b57c87775d0005d1cb5a9b5958ea8f47603
Roman <R.rybkin94@gmail.com>
2022-07-19 10:30:00 +0400

    Переделка проекта под react, прописываем import

--- src/App.js
+++ src/App.js
@@ -1,55 +1,53 @@
-import logo from './logo.svg';
+import logo from '../src/images/logo1.svg';
+import profilePhoto from '../src/images/profilePhoto.png';
+import partnersOne from '../src/images/partnersOne.svg';
+import partnersTwo from '../src/images/partnersTwo.svg';
+import partnersFree from '../src/images/partnersFree.svg';
+import partnersFour from '../src/images/partnersFour.svg';
 import './App.css';
 
 function App() {
   return (
     <div class="page">
+                    <header class="page-header">
+                        <div class="logo">
+                        <img class="header-logo" src={logo} alt=""/>
+                        </div>
+                        <nav class="nav-list">
+                            <div class="page-nav-item"><a class="nav-list_anhor "href="#">Обо мне</a></div>
+                            <div class="page-nav-item"><a class="nav-list_anhor "href="#">Услуги</a></div>
+                            <div class="page-nav-item"><a class="nav-list_anhor "href="#">Блог</a></div>
+                            <div class="page-nav-item"><a class="nav-list_anhor "href="#">Закажите звонок</a></div>
+                        </nav>
+                    </header>  
 
-    <header class="page-header">
-        <div class="logo">
-            <img class="header-logo" src="img/logo.svg"></img>
-        </div>
+                    <main class="header-main">
+                      <div class="left-part">
+                          <h1 class="about-me"><strong class="i-am">Я разрабатываю продукты,<br></br></strong> которые радуют ивдохновляют людей</h1>
+                          <p class="me-descriprion">Привет! Меня зовут Джейк, я продуктовый дизайнер из Берлина.
+                          <br></br>Я создаю удобные интерфейсы для быстрорастущих стартапов.</p>
 
-        <nav class="nav-list">
-            <div class="page-nav-item"><a class="nav-list_anhor "href="#">Обо мне</a></div>
-            <div class="page-nav-item"><a class="nav-list_anhor "href="#">Услуги</a></div>
-            <div class="page-nav-item"><a class="nav-list_anhor "href="#">Блог</a></div>
-            <div class="page-nav-item"><a class="nav-list_anhor "href="#">Закажите звонок</a></div>
-            
-        </nav>
+                          <div class="buttons-menu">
+                              <button class="btn-one book-a-call">Заказать звонок</button>
+                              <button class="btn download-CV">Скачать резюме</button>
+                          </div>
+                      </div>
 
-     
-    </header>  
+                      <div class="right-part">
+                          <img class="photo-profile" src={profilePhoto} alt=""/>
+                      </div>
+                    </main>
 
-<main class="header-main">
-    <div class="left-part">
-        <h1 class="about-me"><strong class="i-am">Я разрабатываю продукты,</strong> которые радуют и вдохновляют людей</h1>
-        <p class="me-descriprion">Привет! Меня зовут Джейк, я продуктовый дизайнер из Берлина.
-        Я создаю удобные интерфейсы для быстрорастущих стартапов.</p>
-
-        <div class="buttons-menu">
-            <button class="btn-one book-a-call">Заказать звонок</button>
-            <button class="btn download-CV">Скачать резюме</button>
-        </div>
-    </div>
-
-    <img class="right-part">
-        <img class="photo-profile" src="img/profilePhoto.png">
-    </div>
-</main>
-
-<div class="trusted-by">
-    <p class="partners-text">Нам доверяют</p>
-    <div class="partners-logo">
-        <img class="partner one"  src="img/Partners_one.svg">
-        <img class="partner two"  src="img/Partners_two.svg">
-        <img class="partner free" src="img/Partners_free.svg">
-        <img class="partner four" src="img/Partners_four.svg">
-    </div>
-</div>
-
-
-</div>
+                      <div class="trusted-by">
+                      <p class="partners-text">Нам доверяют</p>
+                        <div class="partners-logo">
+                            <img class="partner"  src={partnersOne} alt=""/>
+                            <img class="partner"  src={partnersTwo} alt=""/>
+                            <img class="partner" src={partnersFree} alt=""/>
+                            <img class="partner" src={partnersFour} alt=""/>
+                        </div>
+                      </div>
+  </div>
   );
 }
 
