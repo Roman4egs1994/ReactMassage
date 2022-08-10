f72279c3c037a2b9f32df07d2db9011eb827d3ea
Roman <R.rybkin94@gmail.com>
2022-08-10 13:09:31 +0400

    edit class in className

--- src/App.js
+++ src/App.js
@@ -5,84 +5,85 @@ import partnersTwo from '../src/images/partnersTwo.svg';
 import partnersFree from '../src/images/partnersFree.svg';
 import partnersFour from '../src/images/partnersFour.svg';
 import './App.css';
+import './scriptTimer.js';
 
 function App() {
   return (
-    <div class="page">
-        <header class="page-header">
-            <div class="logo">
-                <img class="header-logo" src={logo} alt=""/>
+    <div className="page">
+        <header className="page-header">
+            <div сlassName="logo">
+                <img lassName="header-logo" src={logo} alt=""/>
             </div>
-            <nav class="nav-list">
-                <div class="page-nav-item">
-                    <a class="nav-list_anhor "href="/#">Обо мне</a>
+            <nav className="nav-list">
+                <div сlassName="page-nav-item">
+                    <a className="nav-list_anhor "href="/#">Обо мне</a>
                 </div>
-                <div class="page-nav-item">
-                    <a class="nav-list_anhor "href="/#">Услуги</a>
+                <div className="page-nav-item">
+                    <a className="nav-list_anhor "href="/#">Услуги</a>
                 </div>
-                <div class="page-nav-item">
-                    <a class="nav-list_anhor "href="/#">Блог</a>
+                <div className="page-nav-item">
+                    <a className="nav-list_anhor "href="/#">Блог</a>
                 </div>
-                <div class="page-nav-item">
-                    <a class="nav-list_anhor "href="/#">Закажите звонок</a>
+                <div className="page-nav-item">
+                    <a className="nav-list_anhor "href="/#">Закажите звонок</a>
                 </div>
             </nav>
         </header>  
-        <main class="header-main">
-            <div class="left-part">
-                <p class="title_text">
-                <strong class="i-am">Я разрабатываю продукты,<br></br>
+        <main className="header-main">
+            <div className="left-part">
+                <p className="title_text">
+                <strong className="i-am">Я разрабатываю продукты,<br></br>
                 </strong> которые радуют ивдохновляют людей</p>
-                <p class="description_text">Привет! Меня зовут Джейк, я продуктовый дизайнер из Берлина.
+                <p className="description_text">Привет! Меня зовут Джейк, я продуктовый дизайнер из Берлина.
                 <br></br>Я создаю удобные интерфейсы для быстрорастущих стартапов.</p>
-                <div class="buttons-menu">
-                    <button class="btn-one book-a-call">Заказать звонок</button>
-                    <button class="btn download-CV">Скачать резюме</button>
+                <div className="buttons-menu">
+                    <button className="btn-one book-a-call">Заказать звонок</button>
+                    <button className="btn download-CV">Скачать резюме</button>
                 </div>
             </div>
-            <div class="right-part">
-                <img class="photo-profile" src={profilePhoto} alt=""/>
+            <div className="right-part">
+                <img className="photo-profile" src={profilePhoto} alt=""/>
             </div>
         </main>
-        <div class="trusted-by">
-            <p class="partners-text">Нам доверяют</p>
-            <div class="partners-logo">
-                <img class="partner"  src={partnersOne} alt=""/>
-                <img class="partner"  src={partnersTwo} alt=""/>
-                <img class="partner" src={partnersFree} alt=""/>
-                <img class="partner" src={partnersFour} alt=""/>
+        <div className="trusted-by">
+            <p className="partners-text">Нам доверяют</p>
+            <div className="partners-logo">
+                <img className="partner"  src={partnersOne} alt=""/>
+                <img className="partner"  src={partnersTwo} alt=""/>
+                <img className="partner" src={partnersFree} alt=""/>
+                <img className="partner" src={partnersFour} alt=""/>
             </div>
         </div>
 
-    <div class="promotion">
+    <div className="promotion">
         
-        <div class="left_promotion">
-            <div class="promotion_text">
-                <p class="title_text">Акция для новых клиентов</p>
-                <p class = "description_text"> Мы ценим каждого клиента и предлагаем вам стать одним из них на очень выгодных условиях. 
+        <div className="left_promotion">
+            <div className="promotion_text">
+                <p className="title_text">Акция для новых клиентов</p>
+                <p className = "description_text"> Мы ценим каждого клиента и предлагаем вам стать одним из них на очень выгодных условиях. 
                     Каждому, кто закажет доставку питание на неделю, будет предоставлена скидка в размере <span>20%!</span>
                     <br></br>
                     Акция закончится 20 мая в 00:00</p>
             </div>
         </div>
 
-        <div class="right_promotion">
-           <div class="promotion_text"> 
-                <p class="title_text">Осталось до конца акции:</p>
-                <div class="timer">
-                    <div class="timer__block">
+        <div className="right_promotion">
+           <div className="promotion_text"> 
+                <p className="title_text">Осталось до конца акции:</p>
+                <div className="timer">
+                    <div className="timer__block">
                         <span id="days">00</span>
                         дней
                     </div>
-                    <div class="timer__block">
+                    <div className="timer__block">
                         <span id="hours">00</span>
                         часов
                     </div>
-                    <div class="timer__block">
+                    <div className="timer__block">
                         <span id="minutes">00</span>
                         минут
                     </div>
-                    <div class="timer__block">
+                    <div className="timer__block">
                         <span id="seconds">00</span>
                         секунд
                     </div>
