954d1e3dfeb1289ff389d63f387dab8947fa284b
Roman <R.rybkin94@gmail.com>
2022-08-30 14:47:49 +0400

    footer

--- src/App.js
+++ src/App.js
@@ -710,10 +710,36 @@ function App() {
                             <p className='network_text'>Социальные сети</p>
                         </div>
                         <div className='social_network_img'>
-                            <img className='telegram' src={telegram} alt='' />
-                            <img className='vk' src={vk} alt='' />
-                            <img className='instagram' src={instagram} alt='' />
+                            <a href="https://t.me/massage_ul"><img className='telegram' src={telegram} alt='' /></a>
+                            <a href="https://vk.com/feed"><img className='vk' src={vk} alt='' /></a>
+                            <a href="https://www.instagram.com"><img className='instagram' src={instagram} alt='' /></a>
                         </div>
+                    </div>
+
+                    <div className='footer_right'>
+                            <div className='nav_list_two'>
+                                <nav className='page_nav_item_two'>
+                                    <div className='nav-list_anhor_two'><a className="nav-list_anhor_two "href="/#">Обо мне</a></div>
+                                    <div className='nav-list_anhor_two'><a className="nav-list_anhor_two "href="/#">Контакты</a></div>
+                                    <div className='nav-list_anhor_two'><a className="nav-list_anhor_two "href="/#">Dribbble</a></div>
+                                </nav>
+                            </div>
+
+                            <div className='nav_list_two'>
+                                <nav className='page_nav_item_two'>
+                                    <div className='nav-list_anhor_two'><a className="nav-list_anhor_two "href="/#">Услуги</a></div>
+                                    <div className='nav-list_anhor_two'><a className="nav-list_anhor_two "href="/#">Блог</a></div>
+                                    <div className='nav-list_anhor_two'><a className="nav-list_anhor_two "href="/#">Инстаграм</a></div>
+                                </nav>
+                            </div>
+
+                            <div className='nav_list_two'>
+                                <nav className='page_nav_item_two'>
+                                    <div className='nav-list_anhor_two'><a className="nav-list_anhor_two "href="/#">Опыт</a></div>
+                                    <div className='nav-list_anhor_two'><a className="nav-list_anhor_two "href="/#">Проекты</a></div>
+                                    <div className='nav-list_anhor_two'><a className="nav-list_anhor_two "href="/#">Твиттер</a></div>
+                                </nav>
+                            </div>
                     </div>
                 </div>    
             </div>
